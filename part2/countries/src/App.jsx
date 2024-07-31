0d674087d7d0ebeff6aa1073bfcb9ed6d0fbfadb
kostasp31 <kostaspetrakis2003@gmail.com>
2024-07-31 21:35:40 +0300

    1.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+// To use api 
+// set "VITE_WEATHER_KEY=3008005a520dc732ff0da8c60bb72599" && npm run dev
+// from source: const api_key = import.meta.env.VITE_WEATHER_KEY
+
 const Language = ({lang}) => {
   return (
     <li>{lang}</li>
@@ -20,15 +24,56 @@ const LanguageList = ({langs}) => {
   )
 }
 
+const WeatherInfo = ({city}) => {
+  
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    console.log('hi')
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_WEATHER_KEY}`
+    axios
+      .get(weatherUrl)
+      .then (
+        (response) => {
+          setWeather(response.data)
+          console.log(response.data)
+          console.log('a')
+        }
+      )
+      .catch(
+        (error)=> {
+          console.log("Error")
+          return null
+        }
+      )
+  },[])
+
+
+  if (weather === null) 
+    return null
+
+  const icon = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+  return (
+    <div>
+      <h1>Weather in {city}</h1>
+      <p>Temperature: {weather.main.temp}<sup>o</sup>C</p>
+      <img src={icon} />
+      <p>Wind: {weather.wind.speed}m/s</p>
+    </div>
+  )
+  
+}
+
 const InfoCountry = ({country}) => {
   // console.log(country.languages)
   return (
     <div>
       <h1>{country.name.common}</h1>
       <p>Capital: {country.capital}</p>
-      <p>Area: {country.area} km<sup>2</sup></p>
+      <p>Area: {country.area} km<sup>2</sup></p>  
       <LanguageList langs={country.languages} />
       <img src={country.flags.png} />
+      <WeatherInfo city={country.capital} />
     </div>
   )
 }
